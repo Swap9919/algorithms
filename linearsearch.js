aa54923cb40c832e1af75a29cea0140f882b6896
Sai Swaroop <saiswaroop874@gmail.com>
2023-04-26 15:17:37 +0530

    d

--- linearsearch.js
+++ linearsearch.js
@@ -7,3 +7,5 @@ function linearsearch (arr, target){
 console.log(linearsearch([1,4,6,3,7,9,],7));
 console.log(linearsearch([1,4,6,3,7,9,],6));
 console.log(linearsearch([1,4,6,3,7,9,],16));
+
+// loop over each element in the array, and check to see if the current element is equal to the target, time complexity is Best O(1), average O(n/2), worst O(n).
